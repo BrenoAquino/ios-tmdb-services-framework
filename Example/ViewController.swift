c3f55196692e6d530734a1bb75ff6521db4d3552
Breno Aquino <brenoaquino7phr@gmail.com>
2020-12-07 12:04:20 -0300

    create example target

---
+++ Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMDBServicesExample
+//
+//  Created by Breno Aquino on 07/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
